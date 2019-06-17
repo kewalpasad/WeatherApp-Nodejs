5020b85f1e8dc614e6c967b2187cb0e6ab9c2857
Kewal Pasad <kewal2123@gmail.com>
2019-06-17 14:33:51 -0400

    display the forcast and location data on client

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,25 @@
-fetch('http://localhost:3000/weather?location=!').then(response => {
-	response.json().then(data => {
-		if (data.error) {
-			console.log(data.error);
-		} else {
-			console.log(data.location);
-			console.log(data.forecast);
-		}
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.getElementById('message-1');
+const messageTwo = document.getElementById('message-2');
+
+weatherForm.addEventListener('submit', e => {
+	const location = search.value;
+
+	fetch(`http://localhost:3000/weather?location=${location}`).then(response => {
+		response.json().then(data => {
+			if (data.error) {
+				messageOne.innerHTML = data.error;
+				messageTwo.innerText = '';
+				// console.log(data.error);
+			} else {
+				messageOne.innerHTML = data.location;
+				messageTwo.innerHTML = data.forecast;
+				// console.log(data.location);
+				// console.log(data.forecast);
+			}
+		});
 	});
+
+	e.preventDefault();
 });
